49bf59aaea2b867389447041a77820c73645889d
Mourad Hilmi <mourad.hilmi2@gmail.com>
2016-07-21 14:39:54 +0100

    Model classes

--- src/main/java/control/open/FamillesPrestation.java
+++ src/main/java/control/open/FamillesPrestation.java
@@ -15,6 +15,7 @@ import utilities.ExceptionHandler;
 import java.util.HashMap;
 import java.util.List;
 
+
 /**
  * Created by mourad on 7/21/2016.
  */
@@ -88,41 +89,7 @@ public class FamillesPrestation {
     private static final int _ALL = 1;
     private static final int _SUPERS = 2;
     private static final int _ONE_BY_ID=3;
-    /**UTILITY Methodes**/
-    private ResponseEntity<String> getService(int style,String value){
-        List<FamillePrestation> listFamilles = null;
-        FamillePrestation fp = null;
-
-        ObjectMapper om = new ObjectMapper();
-        try{
-            if(style == _ALL || style==_SUPERS){
-                listFamilles = style == _ALL ? FamillePrestationDAO.getAll() : FamillePrestationDAO.getAllSuper();
-            }
-            else if(style==_ONE_BY_CODE || style == _ONE_BY_ID){
-                fp  = style==_ONE_BY_ID ? FamillePrestationDAO.getFamillePrestation(Long.parseLong(value)):FamillePrestationDAO.find(value);
-            }
-            else{
-                throw new IllegalArgumentException();
-            }
-
-            if((listFamilles==null && (style==_ALL || style==_SUPERS))){
-                HashMap<String,String> error = new HashMap<String, String>();
-                error.put("reason","database exception");
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(om.writeValueAsString(error));
-            }
-
-            if(fp==null&& (style==_ONE_BY_CODE || style==_ONE_BY_ID)){
-                HashMap<String,String> error = new HashMap<String, String>();
-                error.put("reason","no data found");
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(om.writeValueAsString(error));
-            }
-
-            return ResponseEntity.ok(om.writeValueAsString(style==_ALL || style == _SUPERS ? listFamilles:fp));
-        }catch (Exception e){
-            ExceptionHandler.handleException("unkown exception at open/FamillesPrestation::getService",e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"reason\":\"unkown exception\"}exception");
-        }
-    }    private static final int _ONE_BY_CODE=4;
+    private static final int _ONE_BY_CODE=4;
 
 
 }
